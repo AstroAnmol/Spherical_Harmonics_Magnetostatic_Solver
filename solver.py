a0b24f667e333b9570491ef73e80acbe2f167820
AerospaceNerd <anmolsikka09@gmail.com>
2021-12-29 13:34:05 -0500

    saving csv

--- solver.py
+++ solver.py
@@ -9,18 +9,24 @@ susc = 1.0 # Magnetic susceptibility
 a = 1.0 # Grain radius, meters
 sep=2.0    
 alpha=0.0
-L=40 # Number of multipoles used
+L=80 # Number of multipoles used
 debug_mag=1
 
 # f=spherical_harmonic_two_grain(B0,susc, a, sep, alpha, L)
 
 # print(f)
-fmag_0_1=np.zeros(12)
+fmag=np.zeros(12)
 
-sep=np.arange(2.0, 4.4, 0.2)
-susc=12
-for i in range(13):
+sep=np.arange(2.0, 4.3, 0.2)
+susc=75
+check=range(12)
+
+for i in range(12):
     print(i+1)
     f=spherical_harmonic_two_grain(B0,susc, a, sep[i], alpha, L)
-    fmag_0_1[i]=f[2]
+    fmag[i]=f[2]
     print(f[2])
+
+data_d={'Force':fmag, 'Separation':sep}
+data=pd.DataFrame(data=data_d)
+data.to_csv('fmag_susc_75_th_0_L_80.csv')
