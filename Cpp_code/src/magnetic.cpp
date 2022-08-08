da0e3f2a404f83df84e6e6932f02a81eb120b254
AstroAnmol <anmolsikka09@gmail.com>
2022-08-08 19:24:37 -0400

    linear system solver writtem

--- Cpp_code/src/magnetic.cpp
+++ Cpp_code/src/magnetic.cpp
@@ -18,7 +18,7 @@ magnetic::magnetic(double arr[]){
     double H_perp=hmag*std::sin(alpha);
     double H_prll=hmag*std::cos(alpha);
 
-    
+    Eigen::VectorXd Beta1_0(L), Beta2_0(L), Beta1_1(L), Beta2_1(L)
 
     for (int m= 0; m < 2; m++)
     {
@@ -40,10 +40,37 @@ magnetic::magnetic(double arr[]){
         }
         // 2L X 2L Matrix
         Eigen::MatrixXd Am(2*L, 2*L);
+        Am.block(0,0,L,L)=X;
+        Am.block(0,L,L,L)=Delta_m;
+        Am.block(L,0,L,L)=Gamma_m;
+        Am.block(L,L,L,L)=X;
 
+        //qm vector
+        Eigen::VectorXd qm(L);
+        if (m==0){
+            qm(0)=-H_prll*std::pow(a,3)*(1-mu/mu0);
+        }
+        else if (m==1){
+            qm(0)=H_perp*std::pow(a,3)*(1-mu/mu0);
+        }
         
+        # 2L Q vector
+        Eigen::VectorXd Qm(2*L);
+        Qm.block(0,0,L,1)=qm;
+        Qm.block(L,0,L,1)=qm;
+
+        //solve linear system
+        Beta_m=A.PartialPivLU().solve(Qm);
+        if (m==0){
+            Beta1_0=Beta_m.block(0,0,L,1);
+            Beta2_0=Beta_m.block(L,0,L,1);
+        }
+        else if (m==1){
+            Beta1_1=Beta_m.block(0,0,L,1);
+            Beta2_1=Beta_m.block(L,0,L,1);
+        }
     }
-    
+    std::cout<< "Linear System Solved"<<std::endl;
     
 }
 
